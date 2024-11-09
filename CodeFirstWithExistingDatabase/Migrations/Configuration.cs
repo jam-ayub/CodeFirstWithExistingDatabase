9fc7bc9908684fd001cda3778c88cf53280d43cc
Jam <jamayub098@gmail.com>
2024-11-09 16:42:39 +0500

    seeding to database

--- CodeFirstWithExistingDatabase/Migrations/Configuration.cs
+++ CodeFirstWithExistingDatabase/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 ﻿namespace CodeFirstWithExistingDatabase.Migrations
 {
     using System;
+    using System.Collections.ObjectModel;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,6 +15,16 @@
 
         protected override void Seed(CodeFirstWithExistingDatabase.PlutoContext context)
         {
+            context.Authors.AddOrUpdate(a => a.Name,
+                new Author 
+                { 
+                    Name = "Author 1",
+                    Courses = new Collection<Course>()
+                    {
+                        new Course { Name ="Course for Author 1", Description="Description" }
+                    }
+                }
+                );
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
