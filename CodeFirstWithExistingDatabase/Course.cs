a0f071c84e2c77941c117d5771b4a7445b546642
Jam <jamayub098@gmail.com>
2024-11-09 16:03:11 +0500

    Delete Categories Table

--- CodeFirstWithExistingDatabase/Course.cs
+++ CodeFirstWithExistingDatabase/Course.cs
@@ -21,8 +21,6 @@ namespace CodeFirstWithExistingDatabase
 
         public string Description { get; set; }
 
-        public Category Category { get; set; }
-
         public int Level { get; set; }
 
         public float FullPrice { get; set; }
